f0c8329a6b77485d97e537eb4b502b997deb47bf
Simon Gomizelj <simongmzlj@gmail.com>
2012-03-09 15:03:53 -0500

    Reoganized challenge 2.

--- pythonchallenge.com/01.hs
+++ pythonchallenge.com/01.hs
@@ -2,22 +2,27 @@ import Control.Applicative
 import Control.Monad
 import Data.Function
 import Data.Char
+import Data.Map (Map)
 import Data.Maybe
 import qualified Data.Map as M
+
+type Cipher = Map Char Char
 
 rotate :: [a] -> Int -> [a]
 rotate xs n = take (length xs) . drop n $ cycle xs
 
-cipher :: Char -> Char -> [(Char, Char)]
-cipher a b = alph `zip` rotate alph (a /- b)
-  where
-    alph = ['a'..'z']
-    (/-) = abs .: on (-) ord
+cipher :: Int -> Cipher
+cipher step = M.fromList $ alph `zip` rotate alph step
+  where alph = ['a'..'z']
+
+substitute :: Cipher -> Char -> Char
+substitute = ap fromMaybe . flip M.lookup
 
 main :: IO ()
-main = decipher . lines <$> getContents >>= mapM_ putStrLn
-  where
-    cMap     = M.fromList $ cipher 'o' 'q'
-    decipher = fmap . fmap . ap fromMaybe $ flip M.lookup cMap
+main = fmap decipher . lines <$> getContents >>= mapM_ putStrLn
+  where decipher = fmap . substitute . cipher $ step 'o' 'q'
+
+step :: Char -> Char -> Int
+step = abs .: on (-) ord
 
 a .: b = (a .) . b
