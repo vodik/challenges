6ad2b12693d06292c7ffced1a587fae4aacaa3d4
Simon Gomizelj <simongmzlj@gmail.com>
2012-03-09 14:28:08 -0500

    Simplified problem 2.

--- pythonchallenge.com/02.hs
+++ pythonchallenge.com/02.hs
@@ -5,16 +5,17 @@ import Data.Set (Set)
 import qualified Data.Map as M
 import qualified Data.Set as S
 
-freqMap :: String -> Map Char Int
+type FrequencyMap = Map Char Int
+type CharSet = Set Char
+
+freqMap :: String -> FrequencyMap
 freqMap = foldr (M.alter update) M.empty
   where update (Just x) = Just $ x + 1
         update Nothing  = Just 1
 
-lowFreq :: Map Char Int -> Int -> Set Char
-lowFreq m c = S.fromList . fmap fst . M.toList $ M.filter (not . (> c)) m
+lowFreq :: FrequencyMap -> Int -> CharSet
+lowFreq m c = S.fromList . fmap fst . M.toList $ M.filter (<= c) m
 
 main :: IO ()
-main = getContents >>= \str ->
-    let freq = freqMap str
-        set  = lowFreq freq 10
-    in putStrLn $ filter (`S.member` set) str
+main = freqFilter <$> getContents >>= putStrLn
+  where freqFilter = flip S.member . flip lowFreq 10 . freqMap >>= filter
