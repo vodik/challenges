ee48a5153968acb6ab9861b1a68a93ef001c6e2c
Simon Gomizelj <simongmzlj@gmail.com>
2012-03-13 16:01:20 -0400

    More efficient compress pass for optimizer.

--- brainfuck/Brainfuck/Optimizer.hs
+++ brainfuck/Brainfuck/Optimizer.hs
@@ -1,13 +1,14 @@
-module Brainfuck.Optimizer where
+module Brainfuck.Optimizer ( optimize ) where
 
 import Brainfuck.Parser
 
 compress :: [Op] -> [Op]
-compress (Op x:xs) =
-    let c = length $ takeWhile (== Op x) xs
-    in OpN (c + 1) x : compress (drop c xs)
 compress (Loop l:xs) = Loop (compress l) : compress xs
-compress []     = []
+compress (op:xs)     = let (l, r) = break (/= op) xs in merge l op : compress r
+compress []          = []
+
+merge :: [Op] -> Op -> Op
+merge xs (Op x) = OpN (length xs + 1) x
 
 optimize :: [Op] -> [Op]
 optimize = compress
