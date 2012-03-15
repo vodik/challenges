4baa95c1658fecdc125e683f7a68e0d2fc3e2b5e
Simon Gomizelj <simongmzlj@gmail.com>
2012-03-15 03:08:31 -0400

    Add support for adding comments into the output.

--- hs2bf/hs2bf.hs
+++ hs2bf/hs2bf.hs
@@ -2,6 +2,7 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.State
 import Control.Monad.Writer.Strict
+import Data.Char
 
 data Memory = Memory
     { position :: Int
@@ -10,6 +11,7 @@ data Memory = Memory
 
 data Op = Op Int Char
         | Loop [Op]
+        | Comment String
         deriving (Read, Show)
 
 type Index = Int
@@ -21,6 +23,9 @@ raw   = tell . fmap (Op 1)
 cmd c = tell . return . (`Op` c)
 loop  = censor $ return . Loop
 
+clean   = filter $ not . (`elem` "+-<>,.[]")
+comment = tell . return . Comment . clean
+
 goto (Cell new) = do
     pos <- gets $ subtract new . position
     when (pos /= 0) $ do
@@ -30,6 +35,7 @@ goto (Cell new) = do
         modify $ \mem -> mem { position = new }
 
 output x = goto x >> raw "."
+input  x = goto x >> raw ","
 zero   x = goto x >> loop (raw "-")
 
 inc   x n = goto x >> cmd '+' n
@@ -46,13 +52,14 @@ cell = do
     when dirty $ zero c
     return c
 
+x >-> y = while x $ inc y 1
+
 -- modify x y = do
 --     t <- tmp
 --     while y $ do
 --        inc x
 --        inc t
---     while t $
---        inc y
+--     while t $ inc y
 
 -- x `assign` y = zero x >> x `add` y
 
@@ -65,14 +72,29 @@ render = foldr toC mempty
   where
     toC (Op n x) xs = join [ replicate n x, xs ]
     toC (Loop x) xs = '[' : join [ render x, ']' : xs ]
+    toC (Comment str) xs = '\n' : join [ str, "\n", xs ]
 
 program = (`evalState` emptyMem) . execWriterT $ do
     a <- cell
     b <- cell
     c <- cell
-    mapM_ output [ a, b, c ]
-    while b $ do
-        inc c 3
-        output c
+    d <- cell
+    e <- cell
+    comment "input:"
+    input a
+    input d
+    comment "copy:"
+    while a $ do
+        inc b 1
+        inc c 1
+    comment "prefix:"
+    output b
+    inc e $ ord ':'
+    output e
+    comment "replicate:"
+    dec c $ ord '0'
+    while c $ do
+        output d
+        inc d 1
 
 main = putStrLn $ render program
